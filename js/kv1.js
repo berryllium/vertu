dc3d2ed99f2e2346de96f41446229ba3776717a0
berryllium <gorkundp@yandex.ru>
2019-11-01 18:26:05 +0300

    вставил элемент к каждому объекту

--- js/kv1.js
+++ js/kv1.js
@@ -2,3 +2,13 @@
 console.log('подключен скрипт быстрого просмотра')
 arr = document.querySelectorAll('.catalog>.flex')
 console.log(arr.length)
+arr.forEach(element => {
+  element.insertAdjacentHTML('beforeend', '<p>Привет</p>');
+  element.addEventListener('click', ()=>alert('test'))
+});
+
+let kv1 = 	` <div class="kv1">
+<div class="kv1__shadow"></div>
+<div class="kv1__text">Быстый просмотр</div>
+<div class="owl-carousel"></div>
+</div>`
