cecb49f3d878c69a6f03afa60ce8bc824fcdcb43
berryllium <gorkundp@yandex.ru>
2019-11-02 14:43:16 +0300

    добавил slick  и номера

--- js/kv1.js
+++ js/kv1.js
@@ -3,16 +3,16 @@
 // выбираем все блоки с товарами на главноей
 arr = document.querySelectorAll('.catalog>.flex')
 
-// шаблон затемненного блока
-let kv1 = 	` <div class="kv1 hidden">
-<div class="kv1__shadow"></div>
-<div class="kv1__text">Быстый просмотр</div>
-</div>`
-
-// let kv2 = 
 
 // добавляем блоки и показываем при наведении мышки
-arr.forEach(element => {
+for (let i = 0; i < arr.length; i++) {
+  let element = arr[i]
+  // шаблон затемненного блока
+  let kv1 = 	` <div class="kv1 hidden">
+  <div class="kv1__shadow"></div>
+  <div class="kv1__text" data-id = "${i+1}">Быстый просмотр</div>
+  </div>`
+
   element.insertAdjacentHTML('beforeend', kv1);
   $(element).children('div:first').mouseover(() => {
     const kv1 = $(element).children('.kv1')
@@ -22,7 +22,15 @@ arr.forEach(element => {
       kv1.addClass('hidden')
     })
   })
-});
+}
 
+$('.kv1__text').click(() => $('.kv2').removeClass('hidden'))
+
+$('.kv2').on('click', (event) => {
+  console.log(event.target)
+  if (event.target.classList.contains('bg')) {
+    $('.kv2').addClass('hidden')
+  }
+})
 
 
