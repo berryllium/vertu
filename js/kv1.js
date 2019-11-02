5979c50e430116c81da7809668fb9b112e53ad7d
berryllium <gorkundp@yandex.ru>
2019-11-02 13:07:18 +0300

    правая сторона модалки

--- js/kv1.js
+++ js/kv1.js
@@ -1,13 +1,17 @@
 // скрипт быстрого просмотра
 
-
+// выбираем все блоки с товарами на главноей
 arr = document.querySelectorAll('.catalog>.flex')
 
+// шаблон затемненного блока
 let kv1 = 	` <div class="kv1 hidden">
 <div class="kv1__shadow"></div>
 <div class="kv1__text">Быстый просмотр</div>
 </div>`
 
+// let kv2 = 
+
+// добавляем блоки и показываем при наведении мышки
 arr.forEach(element => {
   element.insertAdjacentHTML('beforeend', kv1);
   $(element).children('div:first').mouseover(() => {
@@ -19,4 +23,6 @@ arr.forEach(element => {
     })
   })
 });
+
+
 
