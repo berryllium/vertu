8523dae86da0ae71e215075f1e3b1c64c3dab9f7
berryllium <gorkundp@yandex.ru>
2019-11-02 11:12:18 +0300

    появление kv1

--- js/kv1.js
+++ js/kv1.js
@@ -1,13 +1,22 @@
 // скрипт быстрого просмотра
-console.log('подключен скрипт быстрого просмотра')
+
+
 arr = document.querySelectorAll('.catalog>.flex')
-console.log(arr.length)
-let kv1 = 	` <div class="kv1">
+
+let kv1 = 	` <div class="kv1 hidden">
 <div class="kv1__shadow"></div>
 <div class="kv1__text">Быстый просмотр</div>
 </div>`
+
 arr.forEach(element => {
   element.insertAdjacentHTML('beforeend', kv1);
-  element.addEventListener('click', ()=>alert('test'))
+  $(element).children('div:first').mouseover(() => {
+    const kv1 = $(element).children('.kv1')
+    kv1.removeClass('hidden')
+    kv1.mouseleave(() => {
+      console.log('out')
+      kv1.addClass('hidden')
+    })
+  })
 });
 
